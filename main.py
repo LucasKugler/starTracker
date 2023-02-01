8d6ea1bd5f8e30610c4015d2794a3362b3935b2a
Lucas Kugler <lucas.kugler@gmail.com>
2023-02-02 00:54:49 +0100

    Used provided transforms

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, division
 import os
 import torch
-import pandas as pd
 from skimage import io, transform, color
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,37 +10,7 @@ from torchvision import transforms, utils
 from celestiaDataset import *
 
 
-celestia_dataset = celestiaDataset(root_dir='samples/Set1')
+celestia_dataset = celestiaDataset(root_dir='samples/Set1',transform=transforms.ToTensor())
 
-fig = plt.figure()
-
-for i in range(len(celestia_dataset)):
-    sample = celestia_dataset[i]
-
-    print(i, sample['image'].shape, sample['coords'].shape)
-
-    ax = plt.subplot(1, 4, i + 1)
-    plt.tight_layout()
-    ax.set_title('Sample #{}'.format(i))
-    ax.axis('off')
-    plt.imshow(sample["image"])
-
-    if i == 3:
-        plt.show()
-        break
-
-
-transformed_dataset = celestiaDataset(root_dir='samples/Set1',
-                                   transform=transforms.Compose([
-                                               ToGrayscale(),
-                                               ToTensor()
-                                    ]))
-
-
-for i in range(len(transformed_dataset)):
-    sample = transformed_dataset[i]
-
-    print(i, sample['image'].size(), sample['coords'].size())
-
-    if i == 3:
-        break
+dataloader = DataLoader(celestia_dataset, batch_size=100,
+                        shuffle=True, num_workers=0)
