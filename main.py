993971b0f5249f963b8bf1850dd7110821fe971a
Lucas Kugler <lucas.kugler@gmail.com>
2023-02-03 12:33:49 +0100

    Implemented CNN class

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from celestiaDataset import *
 from starTModel import *
 
 # Get device for training
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
 #  Initialise Dataset
@@ -35,7 +34,9 @@ trainLoader = DataLoader(trainData, batch_size=batchSize, shuffle=True, num_work
 validateLoader = DataLoader(validateData, batch_size=batchSize, num_workers=0)
 testLoader = DataLoader(testData, batch_size=batchSize, num_workers=0)
 
-model = StarTModel(80*120,3)
+# Create Model
+# model = StarTModel(80*120,80*120,3)
+model = StarTConv()
 model.to(device)
 
 # Training loop
@@ -44,36 +45,50 @@ nSamples = len(trainData)
 nIterations = math.ceil(nSamples/batchSize)
 learningRate = 0.001
 
-loss = nn.MSELoss()
+lossFunction = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(),learningRate)
 
-testImage, testCoords = testData[0]
-testPred = model(testImage)
-print(f'Prediction before training:',
-      f'True Latitude: {testCoords[0][0].tolist():05.4f}, predicted: {testPred[0][0].tolist():05.4f}',
-      f'True Altitude: {testCoords[0][1].tolist():05.4f}, predicted: {testPred[0][1].tolist():05.4f}',
-      f'True Roll:     {testCoords[0][2].tolist():05.4f}, predicted: {testPred[0][2].tolist():05.4f}',
-      sep = "\n")
+# testImage, testCoords = testData[0]
+# testImage = testImage.reshape(-1,80*120).to(device)
+# testCoords = testCoords.to(device)
+# testPred = model(testImage)
+# print(f'Prediction before training:',
+#       f'True Latitude: {testCoords[0][0].tolist():05.4f}, predicted: {testPred[0][0].tolist():05.4f}',
+#       f'True Altitude: {testCoords[0][1].tolist():05.4f}, predicted: {testPred[0][1].tolist():05.4f}',
+#       f'True Roll:     {testCoords[0][2].tolist():05.4f}, predicted: {testPred[0][2].tolist():05.4f}',
+#       sep = "\n")
 
     #   f'True Latitude: {testCoords[0].tolist():3.1f}',
 for epoch in range(nEpochs):
-    for i, (image,coords) in enumerate(trainLoader):
-        image, coords = image.to(device), coords.to(device)
-        Pred = model(image)
-        
-        l = loss(coords, Pred)
+    for i, (images,coords) in enumerate(trainLoader):
+        images = images.reshape(-1,80*120).to(device)
+        coords = coords.to(device)
+        # Forward
+        outputs = model(images)
+        loss = lossFunction(coords, outputs)
 
-        l.backward()
-
+        # Backward
+        optimizer.zero_grad()
+        loss.backward()
         optimizer.step()
 
-        optimizer.zero_grad()
 
-    print(f'Epoch {epoch+1}/{nEpochs}, loss = {l.item():.3f}')
+    print(f'Epoch {epoch+1}/{nEpochs}, loss = {loss.item():.3f}')
+with torch.no_grad():
+    errors = []
+    for i, (images, coords) in enumerate(testLoader):
+        testImages = images.reshape(-1,80*120).to(device)
+        testCoords = coords.to(device)
+        testOutputs = model(testImages)
+        errors[i] = lossFunction(testCoords,testOutputs)
+    print(f'Accuracy = {np.array(errors).mean()}')
 
-testPred = model(testImage)
-print(f'Prediction after training:',
-      f'True Latitude: {testCoords[0][0].tolist():05.4f}, predicted: {testPred[0][0].tolist():05.4f}',
-      f'True Altitude: {testCoords[0][1].tolist():05.4f}, predicted: {testPred[0][1].tolist():05.4f}',
-      f'True Roll:     {testCoords[0][2].tolist():05.4f}, predicted: {testPred[0][2].tolist():05.4f}',
-      sep = "\n")
+
+
+
+# testPred = model(testImage)
+# print(f'Prediction after training:',
+#       f'True Latitude: {testCoords[0][0].tolist():05.4f}, predicted: {testPred[0][0].tolist():05.4f}',
+#       f'True Altitude: {testCoords[0][1].tolist():05.4f}, predicted: {testPred[0][1].tolist():05.4f}',
+#       f'True Roll:     {testCoords[0][2].tolist():05.4f}, predicted: {testPred[0][2].tolist():05.4f}',
+#       sep = "\n")
--- starTModel.py
+++ starTModel.py
@@ -1,12 +1,43 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as f
 
-class StarTModel(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(StarTModel, self).__init__()
+class StarTConv(nn.Module):
+    def __init__(self):
+        super(StarTConv, self).__init__()
         self.flatten = nn.Flatten()
-        self.lin = nn.Linear(input_dim, output_dim)
+        self.conv1 = nn.Conv2d(1,3,5,2)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(3,8,5)
+        self.fc1 = nn.Linear(8*12*7,120)
+        self.fc2 = nn.Linear(120,50)
+        self.fc3 = nn.Linear(50,3)
 
     def forward(self, x):
-        x = self.flatten(x)
-        return self.lin(x)
+        out = self.pool(f.relu(self.conv1(x)))
+        out = self.pool(f.relu(self.conv2(x)))
+        out = out.view(-1, 8*12*7)
+        out = f.relu(self.fc1(out))
+        out = f.relu(self.fc2(out))
+        out = self.fc3(out)
+        
+        return out
+
+class StarTModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(StarTModel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.lin1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.lin2 = nn.Linear(hidden_dim, hidden_dim)
+        self.lin3 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # x = self.flatten(x)
+        out = self.lin1(x)
+        out = self.relu(out)
+        out = self.lin2(out)
+        # out = self.relu(out)
+        out = self.lin3(out)
+        
+        return out
